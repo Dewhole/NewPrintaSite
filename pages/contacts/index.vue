717f4b8aa9800cce51727515eff1587ef063b013
User <dewhole@ubuntu.ubuntu-domain>
2020-12-08 12:00:05 +0700

    section6NotFull

---
+++ pages/contacts/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <div class="links">
+        <nuxt-link to="./portfolio/">r6tku6reku65rk</nuxt-link>
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+/* import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue' */
+
+export default {
+  components: {
+
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,89 +1,498 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
+<div class="main">
+  <div class="section1-bg-image">
+    <header class="header">
+      <div class="header-logo"><img src="/printasite-logo.png" alt=""></div>
+
+        <ul class="header-menu">
+          <div class="dropdown">
+            <button class="dropbtn">Разработка сайтов</button>
+            <div class="dropdown-content">
+              <a href="#">Интернет магазин</a>
+              <a href="#">Корпоративный сайт</a>
+              <a href="#">Landing page</a>
+              <a href="#">Сайт визитка</a>
+              <a href="#">Дизайн сайта</a>
+            </div>
           </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+            <li><a href="">Портфолио</a></li>
+            <li><a href="">Контакты</a></li>
+        </ul>
+  
+      <div class="callback">
+        <a href="">Обратный звонок</a>
+      </div>
+    </header>
+
+      <div class="section1">
+        <div class="block-for-pink">
+          <div class="pink-block">
+            <div class="section1-text">ИНТЕРНЕТ РЕШЕНИЯ <br> ДЛЯ ВАШИХ ЗАДАЧ</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section2-bg-image">
+      <div class="block-for-pink2-5">
+        <div class="pink-block2">
+          <div class="section2-5-content">
+            <div class="section2-5-title"><h2>ЗАКАЗАТЬ ИНТЕРНЕТ-МАГАЗИН:</h2>
+            <p class="section2-5-subtitle">Начни продавать онлайн</p>
+            </div>
+            <div class="section2-5-padding"></div>
+            <div class="section2-5-rows section2-row1">
+              <div class="section2-5-icon section2-row1-icon1"><img src="/time-icon.png" alt=""></div>
+              <div class="section2-5-text section2-row1-text1">Продажи круглосуточно 7 дней в неделю без дополнительных затрат</div>
+            </div>
+            <div class="section2-5-rows section2-row2">
+              <div class="section2-5-icon section2-row2-icon2"><img src="/sale-icon.png" alt=""></div>
+              <div class="section2-5-text section2-row2-text2">Увеличение длины чека и частоты покупок более чем на 20%</div>
+            </div>
+            <div class="section2-5-rows section2-row3">
+              <div class="section2-5-icon section2-row3-icon3"><img src="/many-icon.png" alt=""></div>
+              <div class="section2-5-text section2-row3-text3">Полный контроль расходов, отсутствие финансовых потерь</div>
+            </div>
+            <div class="section2-5-button"><a href="" class="section2-5-button-href">Заказать от 50.000₽</a></div>
+          
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section3-bg-image">
+      <div class="block-for-pink2-5">
+        <div class="pink-block3">
+          <div class="section2-5-content">
+            <div class="section2-5-title"><h2>ЗАКАЗАТЬ КОРПОРАТИВНЫЙ САЙТ:</h2>
+            <p class="section2-5-subtitle">Ваш новый офис в сети</p>
+            </div>
+            <div class="section2-5-padding"></div>
+            <div class="section2-5-rows section2-row1">
+              <div class="section2-5-icon section2-row1-icon1"><img src="/oper-icon.png" alt=""></div>
+              <div class="section2-5-text section2-row1-text1">Больше клиентов - проще коммуникации</div>
+            </div>
+            <div class="section2-5-rows section2-row2">
+              <div class="section2-5-icon section2-row2-icon2"><img src="/bril-icon.png" alt=""></div>
+              <div class="section2-5-text section2-row2-text2">Лучше имидж - крупнее контракты</div>
+            </div>
+            <div class="section2-5-rows section2-row3">
+              <div class="section2-5-icon section2-row3-icon3"><img src="/grom-icon.png" alt=""></div>
+              <div class="section2-5-text section2-row3-text3">Шире охват - больше клиентов</div>
+            </div>
+            <div class="section2-5-button"><a href="" class="section2-5-button-href">Заказать от 40.000₽</a></div>
+          
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section4-bg-image">
+      <div class="block-for-pink2-5">
+        <div class="pink-block4">
+          <div class="section2-5-content">
+            <div class="section2-5-title"><h2>ЗАКАЗАТЬ LANDING PAGE</h2>
+            <p class="section2-5-subtitle">Быстрые продажи в интернет</p>
+            </div>
+            <div class="section2-5-padding"></div>
+            <div class="section2-5-rows section2-row1">
+              <div class="section2-5-icon section2-row1-icon1"><img src="/tet-icon.png" alt=""></div>
+              <div class="section2-5-text section2-row1-text1">Все предложения на одной странице, компактно и эффективно</div>
+            </div>
+            <div class="section2-5-rows section2-row2">
+              <div class="section2-5-icon section2-row2-icon2"><img src="/mon-icon.png" alt=""></div>
+              <div class="section2-5-text section2-row2-text2">Быстрое продвижение, результаты уже через месяц</div>
+            </div>
+            <div class="section2-5-rows section2-row3">
+              <div class="section2-5-icon section2-row3-icon3"><img src="/dollar-icon.png" alt=""></div>
+              <div class="section2-5-text section2-row3-text3">Реальная экономия на сроках и деньгах</div>
+            </div>
+            <div class="section2-5-button"><a href="" class="section2-5-button-href">Заказать от 17.000₽</a></div>
+          
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section5-bg-image">
+      <div class="block-for-pink2-5">
+        <div class="pink-block5">
+          <div class="section2-5-content">
+            <div class="section2-5-title"><h2>ЗАКАЗАТЬ САЙТ ВИЗИТКУ:</h2>
+            <p class="section2-5-subtitle">Расскажите о своих<br> преимуществах клиентам</p>
+            </div>
+            <div class="section2-5-padding"></div>
+            <div class="section2-5-rows section2-row1">
+              <div class="section2-5-icon section2-row1-icon1"><img src="/brow-icon.png" alt=""></div>
+              <div class="section2-5-text section2-row1-text1">Всё о Вашей компании лаконично, удобно, компактно</div>
+            </div>
+            <div class="section2-5-rows section2-row2">
+              <div class="section2-5-icon section2-row2-icon2"><img src="/grom-icon.png" alt=""></div>
+              <div class="section2-5-text section2-row2-text2">Увеличение охвата и привлечение новых клиентов</div>
+            </div>
+            <div class="section2-5-rows section2-row3">
+              <div class="section2-5-icon section2-row3-icon3"><img src="/good-icon.png" alt=""></div>
+              <div class="section2-5-text section2-row3-text3">Идеальное решение для малого бизнеса</div>
+            </div>
+            <div class="section2-5-button"><a href="" class="section2-5-button-href">Заказать от 25.000₽</a></div>
+          
+          </div>
+        </div>
+      </div>
+    </div>
+  <div class="section6">
+    <div class="section6-bg-image">
+      <div class="section6-icon"><img src="/logo-w.png" alt=""></div>
+      <div class="section6-text">Мы убеждены, что при создании сайта нужно учитывать все факторы интересные для клиента, разработанный сайт должен быть удобным, понятным и функциональным для клиента. А система управления сайтом(CMS) должна максимально подходить для ваших задач и помогать в эффективном управлении сайта, а не создавать дополнительные трудности.</div>
+      <div class="section6-subtext">УДОБСТВО. ФУНКЦИОНАЛЬНОСТЬ. КОНВЕРСИЯ</div>
+    </div>
+    <div class="section6-footer">
+      <div class="aaa">
+      <div class="section6-pink-block">
+        <div class="section6-pink-block-icon1"><img src="" alt=""></div>
+        <div class="section6-pink-block-text"></div>
+      </div>
+      <div class="section6-pink-block">
+        <div class="section6-pink-block-icon2"></div>
+        <div class="section6-pink-block-text"></div>        
+      </div>
+      <div class="section6-pink-block">
+        <div class="section6-pink-block-icon3"></div>      
+        <div class="section6-pink-block-text"></div>          
+      </div>
+      </div>
+    </div>
+  </div> 
+
+</div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
 
 export default {
   components: {
-    Logo,
-    VuetifyLogo
+    
   }
 }
+
+
 </script>
+
+<style>
+.main{
+margin: 0;
+    padding: 0;
+    font-family: "Roboto slab", sans-serif;
+    color: #646464;
+    font-size: 16px;
+    line-height: 1.5;
+
+    -webkit-font-smoothing: antialiased;
+    background: #f5f0f0;
+    outline: 0;
+    overflow-x: hidden;
+    overflow-y: auto;
+}
+.header a{
+  text-decoration: none;
+  color: #fff;
+  outline: none;
+  -webkit-transition: color 0.2s ease-in-out;
+  -moz-transition: color 0.2s ease-in-out;
+  -ms-transition: color 0.2s ease-in-out;
+  -o-transition: color 0.2s ease-in-out;
+  transition: color 0.2s ease-in-out;
+}
+/* section 6 */
+.aaa{
+  max-width: 1220px;
+  height: 415px;
+  display: flex;
+  text-align: center;
+  margin: auto;
+}
+.section6-pink-block{
+  background-color: rgba(236, 0, 140, 0.7); 
+  width: 100%;
+  margin-left: 15px;
+  margin-right: 15px;
+  margin-top: 40px;
+  margin-bottom: 40px;
+
+
+}
+.section6-bg-image{
+  background: #f5f0f0;
+  vertical-align: middle;
+  
+}
+.section6-icon{
+  opacity: 0.53;
+  opacity: 0.53;
+  width:80%;
+  text-align: center;
+
+  margin: auto;
+  margin-top: 130px;
+  top: 120px;
+}
+
+.section6-text{
+  opacity: 0.53;
+  max-width: 700px;
+  text-align: center;
+  margin: auto;
+  margin-top: 50px;
+  line-height: 33.5px;
+}
+.section6-subtext{
+  font-weight: 700;
+  font-size: 28px;
+  margin-bottom: 20px;
+  width:100%;
+  text-align: center;
+  padding: 60px 0 60px 0px;
+}
+.section6-footer{
+  background: url(/process.jpg) 1% 10% no-repeat; top: 0%; left: 0px;
+  height: 415px;
+
+
+}
+
+
+/* section 5 */
+.section5-bg-image{
+  height: 1000px;
+  background: url(/block-5.jpg) 10% 10% no-repeat; top: 0%; left: 0px;
+  position: relative; transform: translate3d(0px, 0%, 0px); transition: all 1000ms ease 0s;
+}
+.pink-block5{
+  padding: 50px;
+  background-color: rgba(99, 48, 133, 0.70);
+  max-width: 1020px;
+  height: 712px;
+  position: relative;
+  top: 120px;
+  width: 70%;
+}
+
+/* section 4 */
+.section4-bg-image{
+  height: 1000px;
+  background: url(/block-4.jpg) 100% 100% no-repeat; top: 0%; left: 0px;
+  position: relative; transform: translate3d(0px, 0%, 0px); transition: all 1000ms ease 0s;
+}
+.pink-block4{
+  padding: 50px;
+  background-color: rgba(0, 114, 188, 0.70);
+  max-width: 1020px;
+  height: 712px;
+  position: relative;
+  top: 120px;
+  width: 70%;
+}
+
+
+/* section 3 */
+.pink-block3{
+  padding: 50px;
+  background-color: rgba(160, 65, 13, 0.70);
+  max-width: 1020px;
+  height: 712px;
+  position: relative;
+  top: 120px;
+  width: 70%;
+}
+.section3-bg-image{
+  height: 1000px;
+  background: url(/block-3.jpg) 10% 10% no-repeat; top: 0%; left: 0px;
+  position: relative; transform: translate3d(0px, 0%, 0px); transition: all 1000ms ease 0s;
+
+}
+
+
+/* section 2 */
+.section2-5-button-href{
+  display: inline-block;
+  text-align: center;
+  width: 482px;
+  font-size: 28px;
+  text-decoration: none;
+  color: #fff;
+  font-weight: 700;
+}
+.section2-5-button :hover{
+  background-color: rgba(236, 0, 140, 0.7);
+}
+.section2-5-button{
+  padding: 12px 0;
+  width: 100%;
+  background-color: transparent;
+  border: 3px solid #fff;
+  cursor: pointer;
+  max-width: 482px;
+  margin-top: 40px;
+  min-height: 1px;
+
+}
+.section2-5-padding{
+  margin-top: 40px;
+}
+.section2-5-text{
+  margin-left: 10px;
+}
+.section2-5-rows{
+  display: flex;
+  font-size: 24px;
+  color: #fff;
+  font-family: "Roboto light", sans-serif;
+  font-weight: 300;
+  margin-top: 25px;
+  max-width: 500px;
+
+}
+.section2-5-content{
+  width: 70.33333333%;
+  min-height: 1px;
+  margin-left: auto;
+  margin-right: auto;
+
+}
+.section2-5-subtitle{
+  font-size: 28px;
+  font-family: "Roboto slab", sans-serif;
+  margin-top: 0;
+  color: #fff;
+  font-weight: 700;
+  letter-spacing: 0.05em;
+  text-transform: initial;
+  line-height: 35px;
+}
+.section2-5-title{
+  font-size: 27px;
+  font-family: "Roboto slab", sans-serif;
+  margin-top: 0;
+  color: #fff;
+  font-weight: 700;
+  letter-spacing: 0.075em;
+  text-transform: initial;
+  line-height: 60px;
+}
+.block-for-pink2{
+  vertical-align: middle;
+    
+}
+.pink-block2{
+  padding: 50px;
+  background-color: rgba(122, 0, 38, 0.70);
+  max-width: 1020px;
+  height: 712px;
+  position: relative;
+  top: 120px;
+  width: 75%;
+}
+.section2-bg-image{
+  height: 1000px;
+  background: url(/block-2.jpg) 100% 100% no-repeat; top: 0%; left: 0px;
+  position: relative; transform: translate3d(0px, 0%, 0px); transition: all 1000ms ease 0s;
+
+}
+
+
+/* section1 */
+.callback{
+  position: relative;
+  display: block;
+  float: left;
+  height: 48px;
+  top: 6px;
+  padding: 10px 25px;
+  cursor: pointer;
+  border: 2px solid #fff;
+}
+.block-for-pink{
+
+    vertical-align: middle;
+}
+.pink-block{
+  padding: 65px;
+  background-color: rgba(236, 0, 140, 0.7);
+  max-width: 1170px;
+  margin: auto;
+  position: relative;
+  top: 320px;
+}
+.section1{
+
+}
+.section1-text{
+  font-size: 56px;
+  text-align: center;
+  color: white;
+  font-family: "Roboto slab";
+  font-weight: 700;
+  letter-spacing: 0.05em;
+  
+}
+.section1-bg-image{
+  height: 1000px;
+  background: url(/block-1.jpg) 100% 100% no-repeat; position: absolute; top: 0%; left: 0px;
+  position: relative; transform: translate3d(0px, 0%, 0px); transition: all 1000ms ease 0s;
+}
+.header-menu{
+  display: flex;
+  justify-content: space-between;
+  font-family: "Roboto slab", sans-serif;
+  padding: 18px 20px;
+  min-width: 500px;
+  
+}
+.li{
+  list-style-type: none;
+}
+.header-logo img{
+  width: 87%;
+
+}
+.header{
+  display: flex;
+  justify-content: space-around;
+  background-color: rgba(0, 0, 0, 0.6);
+  visibility: visible;
+  opacity: 1;
+  position: relative;
+  box-sizing: border-box;
+  
+}
+
+/* Dropdown Button */
+
+/* The container <div> - needed to position the dropdown content */
+
+
+/* Dropdown Content (Hidden by Default) */
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f1f1f1;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+}
+
+/* Links inside the dropdown */
+.dropdown-content a {
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+}
+
+/* Change color of dropdown links on hover */
+.dropdown-content a:hover {background-color: #ddd;}
+
+/* Show the dropdown menu on hover */
+.dropdown:hover .dropdown-content {display: block;}
+
+/* Change the background color of the dropdown button when the dropdown content is shown */
+.dropdown:hover .dropbtn {font-size: px;}
+</style>
+
